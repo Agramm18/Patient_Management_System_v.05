069aab708693734456906b4785c84e431cefff20
Agramm18 <grammaleks@gmail.com>
2026-03-02 08:45:53 +0100

    Added to laptop

---
+++ src/MenuScreen.java
@@ -0,0 +1,25 @@
+public class MenuScreen {
+    public void show() {
+        System.out.println("\nPlease Choose One Of the following Options\n");
+
+        //Basic Information
+        System.out.println("1. Add a Patient");
+        
+        //Add Information
+        System.out.println("2. Setup Medical Information");
+        System.out.println("3. Setup Finanicial Information");
+        
+        //View Information
+        System.out.println("4. View Basic Information from the patient");
+        System.out.println("5. View Financial Informations from the patient");
+        System.out.println("6. View Medical Informations from the patient");
+
+        //Edit Information
+        System.out.println("7. Edit Basic Data form the patient");
+        System.out.println("8. Edit Financial Data from the patient");
+        System.out.println("9. Edit Medical Information from the patient");
+
+        //Exit
+        System.out.println("10. Exit\n");
+    }
+}
