8d189140acc8144532fec456f60e5172ae6d6646
Alex <grammaleks@gmail.com>
2026-03-03 13:10:48 +0100

    Changed logic flow

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
@@ -9,12 +10,42 @@ public class Main {
         System.out.println("\n-------Welcome to my Patient Management System .v 5.0 this time in Java-------");
         System.out.println("-------------Made from Agramm18 2026 All Rights reserved---------------\n");
 
+        //Load Login Logic
+
+        Account account = new Account();
+        account.HeaderMSGAccount();
+
+        //Error Handeling with try and catch
+        while (true) {
+            try {
+                account.SetBaseValues(scanner);;
+                break;
+
+            } catch (IllegalArgumentException invalidInput) {
+                System.out.println("\nThere is an error of the Login/Registration logic");
+                System.out.println("The error is: " + invalidInput.getMessage() + "\n");
+            }
+        }
+
+        while (true) {
+            try {
+                account.SetAccount(scanner);
+                break;
+
+            } catch (IllegalArgumentException invalidInput) {
+                System.out.println("\nThere is an error of the Login/Registration logic");
+                System.out.println("The error is: " + invalidInput.getMessage() + "\n");
+            }
+        }
+
+        /*
         //Display MainMenu
         MenuScreen window = new MenuScreen();
         window.show();
 
         menu m = new menu(null);
         m.CollectMenuChoice(scanner);
+        */
 
     }
 }
