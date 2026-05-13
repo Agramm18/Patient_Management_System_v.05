fb5fd48f6a53d9db63fb5b67889367d6f9dc74db
Agramm18 <grammaleks@gmail.com>
2026-05-13 21:59:55 +0200

    Build a class to maintain global acess to the DB connection

---
+++ src/main/java/app/Config/DBManager.java
@@ -0,0 +1,37 @@
+package app.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBManager {
+    private static String sqlUser;
+    private static String sqlPWSD;
+    private static String sqlURL;
+
+    private DBManager() {
+
+    }
+
+    public static void initialize(
+            String user,
+            String password,
+            String URL
+    ) {
+        System.out.println("[INFO] Building global DB Connection");
+        System.out.println("[INFO] Save global DB runtime config");
+        System.out.println("[INFO] Set DB Config");
+
+        sqlUser = user;
+        sqlPWSD = password;
+        sqlURL = URL;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                sqlURL,
+                sqlUser,
+                sqlPWSD
+        );
+    }
+}
