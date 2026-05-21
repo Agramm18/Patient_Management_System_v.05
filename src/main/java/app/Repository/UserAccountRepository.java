b1ba3d02497d6d9238e18af0e96bc2b8986010e3
Agramm18 <grammaleks@gmail.com>
2026-05-21 16:07:24 +0200

    Added Folder for DB Actions

---
+++ src/main/java/app/Repository/UserAccountRepository.java
@@ -0,0 +1,42 @@
+package app.Repository;
+
+import app.Config.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserAccountRepository {
+
+    public void newAccount(String Username, String Email, String PhoneNumber, String HashedPWSD) {
+        System.out.println("\n[INFO] Creating new User Account inside the DB");
+
+        try {
+            createUser(Username, Email, PhoneNumber, HashedPWSD);
+        } catch (SQLException error) {
+            System.out.println("Something went wrong: " + error.getMessage());
+        }
+
+    }
+
+    private void createUser(String Username, String Email, String PhoneNumber, String HashedPWSD ) throws SQLException {
+        String sql = "INSERT INTO accounts (account_name, email, phone_number, password_hash) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DBManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            statement.setString(1, Username);
+            statement.setString(2, Email);
+            statement.setString(3, PhoneNumber);
+            statement.setString(4, HashedPWSD);
+
+            int rows = statement.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("\n[OK] User Is Created");
+                System.out.println("[INFO] Rows effected: " + rows);
+            }
+        } catch (SQLException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+}
