92b526e3f16a761a6b6b5f9a7c71f77c00749064
Agramm18 <grammaleks@gmail.com>
2026-05-22 11:57:04 +0200

    Added file to store Login Results if it was a success or what the reason where why not

---
+++ src/main/java/app/Auth/LoginResult.java
@@ -0,0 +1,19 @@
+package app.Auth;
+
+public class LoginResult {
+    private final boolean success;
+    private final String failureReason;
+
+    public LoginResult(boolean success, String failureReason) {
+        this.success = success;
+        this.failureReason = failureReason;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+}
