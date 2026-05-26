21d8579854da387656ad1e6c3c186bc660a857db
Agramm18 <grammaleks@gmail.com>
2026-05-26 13:59:07 +0200

    Changed Department Menu

---
+++ src/main/java/app/Menus/DepartmentMenu.java
@@ -0,0 +1,22 @@
+package app.Menus;
+
+public class DepartmentMenu {
+
+    public void Departments() {
+        System.out.println("\n==================================================");
+        System.out.println("                Available Departments");
+        System.out.println("==================================================");
+
+        System.out.println("\n[1] Medical");
+        System.out.println("[2] Emergency");
+        System.out.println("[3] Laboratory");
+        System.out.println("[4] Pharmacy");
+        System.out.println("[5] IT");
+        System.out.println("[6] Security");
+        System.out.println("[7] Finance");
+        System.out.println("[8] Office");
+        System.out.println("[9] Administration");
+        System.out.println("[10] Training");
+        System.out.println("[11] System\n");
+    }
+}
