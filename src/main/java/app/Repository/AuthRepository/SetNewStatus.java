904b02a7b3a39bac91bd3ae86b13a30ed41b6731
Agramm18 <grammaleks@gmail.com>
2026-05-29 15:30:32 +0200

    Added file

---
+++ src/main/java/app/Repository/AuthRepository/SetNewStatus.java
@@ -0,0 +1,11 @@
+package app.Repository.AuthRepository;
+
+import app.Config.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+
+public class SetNewStatus {
+}
