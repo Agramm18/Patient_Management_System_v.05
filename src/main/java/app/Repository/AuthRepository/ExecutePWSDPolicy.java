d9ba790f553f1ad61448a2eeac5ba5857eac1516
Agramm18 <grammaleks@gmail.com>
2026-05-29 15:30:07 +0200

    Added Placeholder file to change the status

---
+++ src/main/java/app/Repository/AuthRepository/ExecutePWSDPolicy.java
@@ -0,0 +1,17 @@
+package app.Repository.AuthRepository;
+
+public class ExecutePWSDPolicy {
+
+
+    public void locked(String Username) {
+
+    }
+
+    public void quarantine(String Username) {
+
+    }
+
+    public void suspicious(String Username) {
+
+    }
+}
