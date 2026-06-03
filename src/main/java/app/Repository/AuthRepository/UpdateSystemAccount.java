2fc37518398a99a5b6a469666beb8b41e3837c86
Agramm18 <grammaleks@gmail.com>
2026-06-03 13:53:12 +0200

    Added sql Querry to update the choosen System account with the pwsd from before

---
+++ src/main/java/app/Repository/AuthRepository/UpdateSystemAccount.java
@@ -0,0 +1,33 @@
+package app.Repository.AuthRepository;
+
+import app.Config.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class UpdateSystemAccount {
+
+    public void sqlQuerry(String Username, String password) {
+
+        String sql = "UPDATE accounts SET password_hash = ? WHERE account_name = ?";
+
+        try (Connection connection = DBManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+        ) {
+            stmt.setString(1, password);
+            stmt.setString(2, Username);
+
+            int rows = stmt.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("\n[OK] The Password from the System Account " + Username + " is updated");
+                System.out.println("[INFO] Rows affected: " + rows);
+            }
+
+        } catch (SQLException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
+}
