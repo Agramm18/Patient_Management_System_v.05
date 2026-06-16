93fcc3a56aeb3c22c6de1a5e723d279023b93577
Agramm18 <grammaleks@gmail.com>
2026-06-16 16:14:11 +0200

    Created CLI Menu for the Admin

---
+++ src/main/java/app/CLIText/Menus/ServiceMenus/AdminMenu.java
@@ -0,0 +1,42 @@
+package app.CLIText.Menus.ServiceMenus;
+
+import app.Auth.Flow.CurrentSession;
+import app.Auth.Flow.Services.LoginService.CurrentUser;
+import app.Config.LogManager;
+
+import app.Config.LogManager.LogType;
+
+import java.util.List;
+
+public class AdminMenu {
+    private int menuSize;
+
+    public void showMenu() {
+        LogManager.log(LogType.AUTH_SUCCESS, "Routed successfully into the admin menu");
+        CurrentUser user = CurrentSession.getCurrentUser();
+
+        System.out.println("\nWelcome Admin: " + user.getUserName());
+        System.out.println("Please choose one of the following options\n");
+
+        List<String> menuItems = List.of(
+            "View current requests",
+            "Load request flow",
+            "View all active user",
+            "View all locked user",
+            "View all quarantined user",
+            "View all suspicious user",
+            "Activate User",
+            "Disable User"
+        );
+
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println("[" + (i + 1) + "] " + menuItems.get(i));
+        }
+
+        this.menuSize = menuItems.size();
+    }
+
+    public int getMenuSize() {
+        return this.menuSize;
+    }
+}
