91132e38793d3b47b962147f91f23f7eaf23f216
Agramm18 <grammaleks@gmail.com>
2026-06-18 16:12:30 +0200

    Creted first Main Programm Class who shows the admin the current requests

---
+++ src/main/java/app/Repository/ServiceRepository/AdminServices/ShowCurrentRequests.java
@@ -0,0 +1,51 @@
+package app.Repository.ServiceRepository.AdminServices;
+
+import app.Config.LogManager;
+import app.Config.LogManager.LogType;
+
+import app.Config.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class ShowCurrentRequests {
+
+    public void CurrentRequests() {
+        String sql = """
+            SELECT
+                a.account_name,
+                d.department_name,
+                am.requested_job,
+                r.role_name
+            FROM access_management am
+            JOIN accounts a
+                ON am.requested_by = a.id
+            JOIN departments d
+                ON am.requested_department = d.id
+            JOIN roles r
+                ON am.requested_role = r.id
+            """;
+
+        try (Connection connection = DBManager.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql);
+        ) {
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.println("\n");
+
+            while (rs.next()) {
+                System.out.println(
+                  "User: " + rs.getString("account_name") +
+                  " | Department: " + rs.getNString("department_name") +
+                  " | Job: " + rs.getString("requested_job") +
+                  " | Role: " + rs.getString("role_name")
+                );
+            }
+
+        } catch (SQLException error) {
+            LogManager.log(LogType.SQL_EXCEPTION, error.getMessage());
+            System.out.println(error.getMessage());
+        }
+    }
+}
