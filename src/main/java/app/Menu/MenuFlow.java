caed86de4f4f41df855bf7bd43172bd723d48b22
Agramm18 <grammaleks@gmail.com>
2026-06-18 16:11:06 +0200

    Added a Class who collects and vaidates your choice

--- src/main/java/app/Menu/MenuFlow.java
+++ src/main/java/app/Menu/MenuFlow.java
@@ -3,33 +3,39 @@ import java.util.Scanner;
 
 import app.Auth.Flow.CurrentSession;
 
+import app.Config.LogManager;
+import app.Config.LogManager.LogType;
+
 public class MenuFlow {
 
-    public boolean chooseOption(Scanner scanner, int ADMIN_MAX_OPTIONS) {
+    public int chooseOption(Scanner scanner, int MAX_OPTIONS) {
 
-        try {
-            String userChoice = scanner.nextLine();
+        LogManager.log(LogType.MENU_INFO, "Starting Menu Choice Collector");
 
-            if (userChoice.isBlank()) {
-                throw new IllegalArgumentException("The Entered value can't be empty");
-            } else {
-                int userChoiceInt = Integer.parseInt(userChoice);
+        while (true) {
+            try {
+                String userChoice = scanner.nextLine();
 
-                if (userChoiceInt < 0 || userChoiceInt > ADMIN_MAX_OPTIONS) {
-                    throw new IllegalArgumentException("The number can't be less than 0 or higher than " + ADMIN_MAX_OPTIONS);
+                if (userChoice.isBlank()) {
+                    throw new IllegalArgumentException("The Entered value can't be empty");
+                } else {
+                    int userChoiceInt = Integer.parseInt(userChoice);
+
+                    if (userChoiceInt <= 0 || userChoiceInt > MAX_OPTIONS) {
+                        throw new IllegalArgumentException("The number can't be less than 0 or higher than " + MAX_OPTIONS);
+                    }
+
+                    LogManager.log(LogType.MENU_SUCCESS, "The User choose Option: " + userChoiceInt);
+                    return userChoiceInt;
                 }
+
+            } catch (NumberFormatException error) {
+                System.out.println(error.getMessage());
+                LogManager.log(LogType.INVALID_INPUT, error.getMessage());
+            } catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+                LogManager.log(LogType.INVALID_INPUT, error.getMessage());
             }
-        } catch (IllegalArgumentException error) {
-            System.out.println(error.getMessage());
-            return false;
         }
-
-        return false;
-
     }
-
-    public void admin() {
-
-    }
-
 }
