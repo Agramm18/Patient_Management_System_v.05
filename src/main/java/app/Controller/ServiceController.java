25d4ef103b631c64814e33058aa1a3d7112819c1
Agramm18 <grammaleks@gmail.com>
2026-06-18 16:10:12 +0200

    Added Service Controller who choose the Logic based on your Menu Choice and User Role

--- src/main/java/app/Controller/ServiceController.java
+++ src/main/java/app/Controller/ServiceController.java
@@ -1,4 +1,44 @@
 package app.Controller;
 
+import app.Menu.MenuValues;
+import app.Config.LogManager;
+import app.Config.LogManager.LogType;
+import app.Repository.ServiceRepository.AdminServices.ShowCurrentRequests;
+
+import java.util.Scanner;
+
 public class ServiceController {
+
+    public void routeService(MenuValues values, Scanner scanner) {
+        int role = values.userRole();
+
+        switch (role) {
+            case 1:
+                startLocalAdminService(values, scanner);
+                break;
+
+            case 2:
+                startAdminService(values, scanner);
+                break;
+        }
+    }
+
+
+    private void startLocalAdminService(MenuValues values, Scanner scanner) {
+        LogManager.log(LogType.SYSTEM_INFO, "Starting LocalAdmin Service");
+    }
+
+    private void startAdminService(MenuValues values, Scanner scanner) {
+        LogManager.log(LogType.SYSTEM_INFO, "Starting Admin Service");
+        int choice = values.userChoice();
+
+        switch (choice) {
+            case 1:
+                LogManager.log(LogType.SYSTEM_INFO, "The admin have chosen to see the current access requests");
+                ShowCurrentRequests show = new ShowCurrentRequests();
+                show.CurrentRequests();
+                break;
+        }
+
+    }
 }
