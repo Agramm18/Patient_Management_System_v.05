b8b291e0386bffcc16f4b79541ab531cbdc50db3
Agramm18 <grammaleks@gmail.com>
2026-06-25 09:48:33 +0200

    Corrected Naming

--- src/main/java/app/Config/SQLValidationService.java
+++ src/main/java/app/Config/SQLValidationService.java
@@ -1,6 +1,5 @@
 package app.Config;
 
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,36 +8,36 @@ import app.Config.LogManager;
 import app.Config.LogManager.LogType;
 
 public class SQLValidationService {
-    private EnvValidationService env;
+    private final EnvValidationService env;
     private String sqlUser;
     private String sqlPassword;
     private String sqlURL;
 
-    //Konstruktor to load env values from EnvValidationService.java
+    //Constructor to load env values from EnvValidationService.java
     public SQLValidationService(EnvValidationService env) {
         this.env = env;
     }
 
-    public boolean DBConnection() {
+    public boolean dbConnection() {
         String host = env.getHost();
         int port = env.getPort();
-        String DBName = env.getDBName();
-        String DBUser = env.getUser();
-        String DBPassword = env.getPassword();
+        String dbName = env.getDBName();
+        String dbUser = env.getUser();
+        String dbPassword = env.getPassword();
 
         System.out.println("[INFO] Building Global SQL Connection");
         LogManager.log(LogType.CONFIG_INFO, "Building Global importable SQL Connection");
-        String URL = "jdbc:mysql://" + host + ":" + port + "/" + DBName; //MySQL Connector URL
+        String URL = "jdbc:mysql://" + host + ":" + port + "/" + dbName; //MySQL Connector URL
 
         System.out.println("[DEBUG] SQL URL: " + URL);
         LogManager.log(LogType.SQL_DEBUG, "URL: " + URL);
 
         //Routing Class variables to method variables
-        this.sqlUser = DBUser;
-        this.sqlPassword = DBPassword;
+        this.sqlUser = dbUser;
+        this.sqlPassword = dbPassword;
         this.sqlURL = URL;
 
-        try (Connection connection = DriverManager.getConnection(URL, DBUser, DBPassword)) {
+        try (Connection connection = DriverManager.getConnection(URL, dbUser, dbPassword)) {
             System.out.println("[OK] SQL is connected successfully");
             LogManager.log(LogType.SQL_OK, "The Database connection established successfully ");
             return true;
@@ -50,11 +49,11 @@ public class SQLValidationService {
     }
 
     //Building getter to maintain a global connection
-    public String getSQLUser() {
+    public String getSqlUser() {
         return sqlUser;
     }
 
-    public String getSqlPWSD() {
+    public String getSqlPassword() {
         return sqlPassword;
     }
 
